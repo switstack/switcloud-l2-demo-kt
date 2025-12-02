2968f1515966602d578b5085efdedbdc83480720
Adrien NEVEU <ane@switstack.io>
2025-12-02 16:26:36 +0100

    implementing preview of Footer

--- switcloud-l2-demo/build.gradle.kts
+++ switcloud-l2-demo/build.gradle.kts
@@ -77,12 +77,13 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
 
     testImplementation("junit:junit:4.13.2")
 
--- switcloud-l2-demo/src/main/java/io/switstack/switcloud/switcloud_l2_demo/Footer.kt
+++ switcloud-l2-demo/src/main/java/io/switstack/switcloud/switcloud_l2_demo/Footer.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -42,4 +43,10 @@ fun Footer(modifier: Modifier = Modifier) {
             contentScale = ContentScale.Fit
         )
     }
+}
+
+@Preview
+@Composable
+fun FooterPreview() {
+    Footer()
 }
