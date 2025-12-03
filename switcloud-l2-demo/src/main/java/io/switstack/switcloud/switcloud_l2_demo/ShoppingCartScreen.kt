07a6ae495c63341d877927c7c07f0ab9ccd7075f
Adrien NEVEU <ane@switstack.io>
2025-12-03 16:38:53 +0100

    feat: Managing dark theme and adding Action component styles

--- switcloud-l2-demo/src/main/java/io/switstack/switcloud/switcloud_l2_demo/ShoppingCartScreen.kt
+++ switcloud-l2-demo/src/main/java/io/switstack/switcloud/switcloud_l2_demo/ShoppingCartScreen.kt
@@ -1,6 +1,6 @@
 package io.switstack.switcloud.switcloud_l2_demo
 
-import androidx.compose.foundation.background
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,10 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices.TABLET
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.switstack.switcloud.switcloud_l2_demo.ui.theme.Switcloudl2demoktTheme
@@ -27,52 +29,55 @@ fun ShoppingCartScreen(
     val items = (1..5).map { it to it * 10.0 }
     val total = items.sumOf { it.second }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    Surface(color = MaterialTheme.colorScheme.background) {
         Column(
             modifier = Modifier
-                .weight(1f)
-                .verticalScroll(rememberScrollState()),
+                .fillMaxSize()
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Shopping Cart")
-            Spacer(modifier = Modifier.height(16.dp))
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Shopping Cart")
+                Spacer(modifier = Modifier.height(16.dp))
 
-            // Hardcoded shopping cart items
-            items.forEach { (item, price) ->
+                // Hardcoded shopping cart items
+                items.forEach { (item, price) ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text("Item $item")
+                        Text("$${price}")
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text("Item $item")
-                    Text("$${price}")
+                    Text("Total")
+                    Text("$$total")
                 }
             }
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text("Total")
-                Text("$$total")
+            Column {
+                Action(buttonText = "Proceed with payment",
+                       buttonType = ButtonType.Filled,
+                       onClick = {
+                           onProceedPaymentClick(total)
+                       })
+                Footer()
             }
-        }
-        Column {
-            Action(buttonText = "Proceed with payment", onClick = {
-                onProceedPaymentClick(total)
-            }
-            )
-            Footer()
         }
     }
 }
 
-@Preview(name = "Default Preview")
+@Preview(device = TABLET)
+@Preview(device = TABLET, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun ShoppingCartPreview() {
     Switcloudl2demoktTheme {
